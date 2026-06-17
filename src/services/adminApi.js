dc00b92f084315ffab69568d669aeb706c6aae48
Launchkind <launchkind.in@gmail.com>
2026-06-17 19:12:45 +0530

    Admin added

--- src/services/adminApi.js
+++ src/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://13.62.104.99/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://api.amzy.co.in/api';
 
 console.log('🔗 Admin Panel API URL:', API_URL);
 
